aa2be30cc802e927ae1d9433a180420bcccf3b17
Shakib448 <shakiba448@gmail.com>
2023-06-21 05:13:22 +0600

    WIP on nestjs initialization

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.enableCors({ credentials: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(8000);
 }
 bootstrap();
