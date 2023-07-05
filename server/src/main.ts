0276330675de3c439d8c19d94f8a1e148e910d54
Shakib448 <shakiba448@gmail.com>
2023-07-05 09:39:25 +0600

    WIP on client

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
-  app.enableCors({ credentials: true });
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(8000);
 }
