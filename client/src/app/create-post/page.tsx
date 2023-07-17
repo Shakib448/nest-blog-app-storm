6d5a10628862317a3f3665fd589533782c5db4b3
Shakib448 <shakiba448@gmail.com>
2023-07-17 09:12:54 +0600

    WIP on authentications

--- client/src/app/create-post/page.tsx
+++ client/src/app/create-post/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import CreatePostComponent from "@/components/createPost";
 import WithSession from "@/utils/HOC/uesSessionEvents";
 import React from "react";
--- client/src/common/Navbar.tsx
+++ client/src/common/Navbar.tsx
@@ -1,9 +1,17 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { signOut } from "@/hooks/signOut";
+import axios from "axios";
 
 const Navbar = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  (async () => {
+    const { data } = await axios.get("/api/getToken");
+    setIsAuth(data?.authenticated);
+  })();
+
   return (
     <div className="navbar bg-base-100 fixed top-0 px-4 shadow-md">
       <div className="flex-1">
@@ -13,37 +21,42 @@ const Navbar = () => {
       </div>
       <div className="flex-none gap-2">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">Posts</Link>
-          </li>
-          <li>
-            <Link href="/create-post">Create Post</Link>
-          </li>
-          <li>
-            <Link href="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link href="/sign-up">Sign Up</Link>
-          </li>
+          {!isAuth && (
+            <>
+              <li>
+                <Link href="/sign-in">Sign In</Link>
+              </li>
+              <li>
+                <Link href="/sign-up">Sign Up</Link>
+              </li>
+            </>
+          )}
         </ul>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link href="/profile">Profile</Link>
-            </li>
-            <li>
-              <a onClick={() => signOut({ callbackUrl: "/sign-in" })}>Logout</a>
-            </li>
-          </ul>
-        </div>
+        {isAuth && (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link href="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link href="/create-post">Create Post</Link>
+              </li>
+              <li>
+                <a onClick={() => signOut({ callbackUrl: "/sign-in" })}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
